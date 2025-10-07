96f7501f9372cfc24f5e81787801052ab9a731bc
alexisstdev <alexissanmiguel03@gmail.com>
2025-10-07 10:00:02 -0500

    refactor: Remove mint call from NFT module for manual execution post-deployment

--- ignition/modules/NFT.ts
+++ ignition/modules/NFT.ts
@@ -3,8 +3,5 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("NFTModule", (m) => {
 	const nft = m.contract("NFT");
 
-	// Remove the mint call since we'll do it manually after deployment
-	// The mintNFT function requires an address parameter which should be set at runtime
-
 	return { nft };
 });
